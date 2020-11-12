ec0d866954a029fac18426c48bdf9f60e5573222
Munira Ahmed <munira.ahmed@learnet.se>
2020-11-12 09:54:35 +0100

    följa psuedokoden/kommentarerna

--- BlackJack/projekt-21/Program.cs
+++ BlackJack/projekt-21/Program.cs
@@ -6,7 +6,26 @@ namespace projekt_21
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Skriv välkomstmeddelande
+            Console.WriteLine("VÄlkommen till 21:an!");
+       
+            // Sätt menyVal till "0"   
+            string menyVal = "0";
+
+            // while (menyval != ”4”)
+            while (menyVal != "4")
+            {
+            // Skriv ut meny
+
+            // Läs in menyVal
+
+            // switch menyVal
+            // case 1: Spela en omgång av 21:an
+            // case 2: Visa senaste vinnaren
+            // case 3: Visa spelets regler
+            // case 4: Gör ingenting (programmet avslutas)
+            // default: Skriv att alternativet var ogiltigt
+            }
         }
     }
 }
