f85abcc45370653fcdd0f58c02d2b69e49677f42
Mona Lisa <munira.ahmed@elev.ga.ntig.se>
2021-01-13 15:00:44 +0100

    repetition

---
+++ Labbar/Expert10000/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Expert10000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ange hur många timmar 
+            Console.Write("Hur många timmar vill du träna per dag? ");
+
+            //Läs in användarens svar
+            string svar = Console.ReadLine();
+            double timmarPerdag = 0;
+
+            //double timmar = (double)svar; //omvandla till ett flyttal, 
+            //kontrollera att användaren matar in ett tal
+            
+            while (!double.TryParse(svar, out timmarPerdag)&& timmarPerdag <= 0)
+            {
+                Console.Write("Oglitigt input,försök igen");
+                svar =  Console.ReadLine();
+            }
+
+            //Räkna ut när man blir klar
+            DateTime datumKlar = DateTime.Today.AddDays(10000 / timmarPerdag);
+            TimeSpan hurLångTid = datumKlar - DateTime.Today;
+
+            // Räkna ut antal år
+            int antalÅr = hurLångTid.Days / 365;
+
+            Console.WriteLine($"Du måste träna i {antalÅr} år, mao är du klar år {datumKlar}");
+        }
+    }
+}
---
+++ Labbar/SlumpBetyg/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace SlumpBetyg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Slumpa fram ett tal 0-5 
+            // Där 0=F, osv
+            //skapa en slumpgenerator
+            Random tärning = new Random();
+
+            //Kasta tärningen
+            int slumptal = tärning.Next(0, 8);
+            Console.WriteLine($"slumptalet = {slumptal}");
+
+            //skriv ut betyg 
+     /*       if (slumptal == 0)
+            {
+                Console.WriteLine("Du fick betyget F!");
+            }
+            if (slumptal == 1)
+            {
+                Console.WriteLine("Du fick betyget E!");
+            }
+            if (slumptal == 2)
+            {
+                Console.WriteLine("Du fick betyget D!");
+            }
+            if (slumptal == 3)
+            {
+                Console.WriteLine("Du fick betyget C!");
+            }
+            if (slumptal == 4)
+            {
+                Console.WriteLine("Du fick betyget B!");
+            }
+            if (slumptal == 5)
+            {
+                Console.WriteLine("Du fick betyget A!");
+            }*/
+
+            
+            //Alternativ 2
+     /*      switch (switch_on)
+            {
+               case 1:
+                    Console.WriteLine("Du fick betyget F!");
+                    break;
+                case 2:
+                    Console.WriteLine("Du fick betyget E!");
+                    break;
+                case 3:
+                    Console.WriteLine("Du fick betyget D!");
+                    break;
+                case 4:
+                    Console.WriteLine("Du fick betyget C!");
+                    break;
+                case 5:
+                    Console.WriteLine("Du fick betyget B!");
+                    break;
+
+                default:
+                 Console.WriteLine("Du fick betyget A!");
+                    break;
+            }*/
+
+            // Alternariv 3
+            string[] betyg = {"F", "E","E","E", "D", "C", "B", "A"}; // större chans a
+            Console.WriteLine($"Du fick {betyg[slumptal]} i slutbetyg") ;
+        }
+    }
+}
